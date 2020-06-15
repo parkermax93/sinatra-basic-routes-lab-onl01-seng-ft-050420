3542850d7ca19a807bd9c443d03e82ac6af39436
Parker Williamson <parker.williamson@gmail.com>
2020-06-15 17:13:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is Parker."
+  end
+  
+  get '/hometown' do 
+    "My hometown is Puyallup."
+  end
+  
+  get '/favorite-song' do 
+    "My favorite song is Popular Monster."
+  end
 end
